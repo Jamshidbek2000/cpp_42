64ec0f4483120272bf818643f4dd4bcbaf34f6f7
jergashe <jergashe@student.42heilbronn.de>
2023-04-24 14:27:19 +0200

    diamondTrap constructor change

--- cpp_03/ex03/src/DiamondTrap.cpp
+++ cpp_03/ex03/src/DiamondTrap.cpp
@@ -7,16 +7,12 @@
 DiamondTrap::DiamondTrap() : ClapTrap("Default_ClatTrap"), ScavTrap(), FragTrap(), _name("Default")
 {
 	std::cout << "DiamondTrap default constructor called" << std::endl;
-	_hit_points = FragTrap::_hit_points;
-	_attack_damage = FragTrap::_attack_damage;
 	_energy_points = ScavTrap::_energy_points;
 }
 
 DiamondTrap::DiamondTrap(std::string const & name) : ClapTrap(name + "_clap_trap"), ScavTrap(), FragTrap(), _name(name)
 {
 	std::cout << "DiamondTrap name constructor called" << std::endl;
-	_hit_points = FragTrap::_hit_points;
-	_attack_damage = FragTrap::_attack_damage;
 	_energy_points = ScavTrap::_energy_points;
 }
 
